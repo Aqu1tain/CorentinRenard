6de939c2ff37356193d03ff04070b88cc3bf7a82
Aquit4in <corentinfox08@gmail.com>
2024-05-07 14:06:51 +0200

    Stylized Title

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,12 +4,13 @@ import {
 } from "react-router-dom";
 
 import './Header.scss';
+import './TitleAnim.scss';
 import ModeSwitch from '../ModeSwitch/ModeSwitch';
 
 export default function Header() {
     return (
         <div className="header">
-            <h2><Link to="/">Corentin Renard</Link></h2>
+            <h2><Link to="/"><span id='corentin'>Corentin</span> <span id='renard'>Renard</span></Link></h2>
             <nav>
                 <ul>                    
                     <li><Link to="/skills">Compétences</Link></li>
